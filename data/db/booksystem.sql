710d9ee22d419d48d9fceaafb46e467ec960e411
wanglu119 <805784956@qq.com>
2020-07-03 01:49:24 +0800

    modify db

--- data/db/booksystem.sql
+++ data/db/booksystem.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50719
  File Encoding         : 65001
 
- Date: 02/07/2020 23:03:21
+ Date: 03/07/2020 01:45:58
 */
 
 SET NAMES utf8mb4;
@@ -33,7 +33,7 @@ CREATE TABLE `admin`  (
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES ('root', '2DFA2F725CECC23403BBEC1E401ABDDB', '2020-07-02 22:59:09', 1, 1);
+INSERT INTO `admin` VALUES ('root', '2DFA2F725CECC23403BBEC1E401ABDDB', '2020-07-03 01:10:49', 1, 1);
 INSERT INTO `admin` VALUES ('yangqifan', '7534F920135A408E4C13F2353D87356A', '2020-07-02 23:00:49', NULL, 1);
 
 -- ----------------------------
@@ -41,49 +41,60 @@ INSERT INTO `admin` VALUES ('yangqifan', '7534F920135A408E4C13F2353D87356A', '20
 -- ----------------------------
 DROP TABLE IF EXISTS `books`;
 CREATE TABLE `books`  (
-  `bid` int(11) NOT NULL,
+  `bid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `creadate` datetime(0) NULL DEFAULT NULL,
+  `credate` datetime(0) NULL DEFAULT NULL,
   `status` int(11) NULL DEFAULT NULL,
   `note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `imagepath` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `iid` int(11) NULL DEFAULT NULL,
+  `aid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`bid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of books
 -- ----------------------------
+INSERT INTO `books` VALUES (1, 'JSP开发指南', '2020-07-03 01:12:01', 1, 'JSP开发指南', NULL, 1, 'root');
+INSERT INTO `books` VALUES (2, 'Java开发指南', '2020-07-03 01:26:43', 1, 'Java开发指南', '/imagepath/1593710803062.jpg', 1, 'root');
 
 -- ----------------------------
 -- Table structure for item
 -- ----------------------------
 DROP TABLE IF EXISTS `item`;
 CREATE TABLE `item`  (
-  `iid` int(11) NOT NULL,
+  `iid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`iid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of item
 -- ----------------------------
+INSERT INTO `item` VALUES (1, '计算机', '计算机图书');
+INSERT INTO `item` VALUES (2, '文学', '文学散文');
 
 -- ----------------------------
 -- Table structure for lenbook
 -- ----------------------------
 DROP TABLE IF EXISTS `lenbook`;
 CREATE TABLE `lenbook`  (
-  `leid` int(11) NOT NULL,
+  `leid` int(11) NOT NULL AUTO_INCREMENT,
   `credate` datetime(0) NULL DEFAULT NULL,
   `retdate` datetime(0) NULL DEFAULT NULL,
   `retday` int(11) NULL DEFAULT NULL,
   `creditno` int(11) NULL DEFAULT NULL,
+  `retstatus` int(11) NULL DEFAULT NULL,
+  `mid` int(11) NULL DEFAULT NULL,
+  `bid` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`leid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of lenbook
 -- ----------------------------
+INSERT INTO `lenbook` VALUES (1, '2020-07-03 01:45:19', NULL, 1, 0, NULL, 123, 1);
 
 -- ----------------------------
 -- Table structure for member
@@ -103,6 +114,6 @@ CREATE TABLE `member`  (
 -- ----------------------------
 -- Records of member
 -- ----------------------------
-INSERT INTO `member` VALUES ('123', '杨启帆', 18, 1, '13811111111', 90, 3);
+INSERT INTO `member` VALUES ('123', '杨启帆', 18, 1, '13811111111', 90, 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
